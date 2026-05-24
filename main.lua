ce15563b5a1558355c18692b80a6c887c8b4f550
Nil-Son <buhayhayahay332@gmail.com>
2026-05-23 19:14:52 -0700

    yeah

--- main.lua
+++ main.lua
@@ -3,6 +3,20 @@ if getgenv().__OP1NIBBLER_UI__ then
 end
 getgenv().__OP1NIBBLER_UI__ = true
 
+local oldStrByte
+oldStrByte = hookfunction(string.byte, newcclosure(function(a0, a1)
+    if checkcaller() or type(a0) ~= "string" or not (a0:sub(1, 1) == "{" and a0:sub(-1) == "}") then
+        return oldStrByte(a0, a1)
+    end
+
+    local luraph = getstack(3, 1)
+    luraph[1] = luraph[2]
+    luraph[5] = #luraph[2]
+    setstack(3, 4, luraph[5])
+
+    return oldStrByte(luraph[1], a1)
+end))
+
 local function import(path)
     local baseUrl = getgenv().OP1NIBBLER_BASE_URL or
                         "https://raw.githubusercontent.com/buhayhayahay332-lang/main/rage"
